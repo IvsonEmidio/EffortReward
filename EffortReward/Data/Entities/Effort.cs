e76650ad99c09a6140effc1e4f272d13bfdc0a9a
IvsonEmidio <contato.ivsonemidio@gmail.com>
2023-01-01 20:25:06 -0300

    feat(effort): add Effort entity

---
+++ EffortReward/Data/Entities/Effort.cs
@@ -0,0 +1,20 @@
+﻿namespace EffortReward.Data.Entities
+{
+    public class Effort
+    {
+        public enum EffortStatus
+        {
+            AVAILABLE,
+            STARTED,
+            FAILED,
+            DONE
+        };
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int RewardPoints { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
