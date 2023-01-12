66813176408cb9e0cf025154981bead444ad9621
IvsonEmidio <contato.ivsonemidio@gmail.com>
2023-01-11 23:41:16 -0300

    docs(effort): add more robust swagger doc on controller

--- EffortReward/Controllers/EffortController.cs
+++ EffortReward/Controllers/EffortController.cs
@@ -1,5 +1,3 @@
-
-
 using EffortReward.Data.Entities;
 using EffortReward.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -18,7 +16,14 @@ namespace EffortReward.Controllers
             _service = effortService;
         }
 
+        /// <summary>
+        /// Find all efforts
+        /// </summary>
+        /// <response code="200">Ok</response>
+        /// <response code="500">Internal server error</response>
         [HttpGet]
+        [ProducesResponseType(typeof(Effort[]), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> All()
         {
             var efforts = await _service.FindAll();
@@ -26,7 +31,16 @@ namespace EffortReward.Controllers
             return Ok(efforts);
         }
 
+        /// <summary>
+        /// Find specific effort by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="200">Found successfully</response>
+        /// <response code="404">Not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpGet("id")]
+        [ProducesResponseType(typeof(Effort), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> FindOne(int id)
         {
             var effort = await _service.FindOne(id);
@@ -39,7 +53,16 @@ namespace EffortReward.Controllers
             return Ok(effort);
         }
 
+        /// <summary>
+        /// Create new effort
+        /// </summary>
+        /// <param name="effort"></param>
+        /// <response code="201">Created successfully</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="500">Internal server error</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Store(Effort effort)
         {
             var isStored = await _service.StoreOne(effort);
@@ -47,7 +70,18 @@ namespace EffortReward.Controllers
             return StatusCode(isStored ? 201 : 500);
         }
 
+        /// <summary>
+        /// Update effort
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="effort"></param>
+        /// <response code="204">Updated successfully</response>
+        /// <response code="400">Bad request</response>
+        /// <response code="404">Item not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpPut("id")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Update(int id, Effort effort)
         {
             try
@@ -65,6 +99,13 @@ namespace EffortReward.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Delete specific effort
+        /// </summary>
+        /// <param name="id"></param>
+        /// <response code="200">Deleted successfully</response>
+        /// <response code="404">Item not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpDelete("id")]
         public async Task<IActionResult> Destroy(int id)
         {
